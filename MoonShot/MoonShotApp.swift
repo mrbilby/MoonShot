ec68aa34b33a5b177cf830e9f58cbd8b5ff5327e
mrbilby <baileymrb@gmail.com>
2023-03-27 21:30:46 +0100

    Initial Commit

---
+++ MoonShot/MoonShotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonShotApp.swift
+//  MoonShot
+//
+//  Created by James Bailey on 27/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct MoonShotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
